89c71c8efd9b42e8256859f50580199b5dad19e9
wubalubadubdub <beargrillsllc@gmail.com>
2019-02-03 00:13:54 -0500

    Finish lecture 115 of Udemy React course

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Layout = (props) => (
+  <React.Fragment>
+    <div>
+      Toolbar, SideDrawer, Backdrop
+    </div>
+    <main>
+      {props.children}
+    </main>
+  </React.Fragment>
+);
+
+export default Layout;
