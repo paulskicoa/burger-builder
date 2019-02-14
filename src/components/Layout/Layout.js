edd9979687d7e869855e3491ab1fcb7abb0b54d2
wubalubadubdub <beargrillsllc@gmail.com>
2019-02-14 12:50:15 -0500

    Finish Lecture 167 of Udemy React course

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
 
 const Layout = (props) => (
   <React.Fragment>
     <div>
-      Toolbar, SideDrawer, Backdrop
+      <Toolbar />
     </div>
     <main className={styles.Content}>
       {props.children}
